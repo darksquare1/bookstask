56d0ce3aa87c730dd5cb9846f7f1f6ddfa3f2445
depressedkid <nekit05@list.ru>
2025-01-11 18:34:57 +1000

    ADD: function to get user from token, routes for login and register

---
+++ app/api/endpoints/user.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
+
+from app.crud.user import create_user
+from app.api import schemas
+from app.db import models
+from app.db.database import get_db
+from utils.security import verify_password, create_access_token
+
+auth_router = APIRouter()
+
+
+@auth_router.post('/register', response_model=schemas.UserCreate)
+def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = create_user(db=db, user=user)
+    if not db_user:
+        raise HTTPException(status_code=400, detail="Username or email already taken")
+    return JSONResponse(content={'details': 'Successfully registered!'})
+
+
+@auth_router.post('/login')
+def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.username == user.username).first()
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail='Invalid username or password')
+
+    access_token = create_access_token(data={'sub': db_user.username})
+    return {'access_token': access_token, 'token_type': 'bearer'}
--- utils/security.py
+++ utils/security.py
@@ -1,6 +1,8 @@
 import datetime
 import jwt
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+
 from app.core.config import settings
 
 from passlib.context import CryptContext
@@ -20,7 +22,7 @@ def verify_password(plain_password: str, hashed_password: str):
 
 
 def create_access_token(data: dict, token_life_time: int = ACCESS_TOKEN_EXPIRE_MINUTES):
-    expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.datetime.now(datetime.UTC) + datetime.timedelta(minutes=token_life_time)
     to_encode = data.copy()
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -30,8 +32,17 @@ def create_access_token(data: dict, token_life_time: int = ACCESS_TOKEN_EXPIRE_M
 def verify_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload.get('sub')
+        return payload
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail='Expired token')
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail='Invalid token')
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = verify_token(token)
+    username = payload.get('sub')
+    return username
