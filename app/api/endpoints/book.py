59d2630e8feeaa84ac3a51c0e9cac3bdffb30693
depressedkid <nekit05@list.ru>
2025-01-13 19:32:43 +1000

    ADD: CRUD for book

---
+++ app/api/endpoints/book.py
@@ -0,0 +1,113 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.api import schemas
+from app.db import models
+from app.db.database import get_db
+from utils.security import RoleVerify
+
+books_router = APIRouter()
+
+
+@books_router.get('', response_model=list[schemas.BookOut])
+def get_books(depends_on=Depends(RoleVerify(['admin', 'reader'])), db: Session = Depends(get_db)):
+    books = db.query(models.Book).all()
+    if not books:
+        raise HTTPException(status_code=404, detail='Books not found')
+
+    return [
+        schemas.BookOut(
+            id=book.id,
+            title=book.title,
+            description=book.description,
+            publication_date=book.publication_date,
+            available_copies=book.available_copies,
+            genres=[genre.name for genre in book.genres],
+            authors=[author.name for author in book.authors]
+        )
+        for book in books
+    ]
+
+
+@books_router.post('', response_model=schemas.BookOut)
+def create_book(book: schemas.BookCreate, depends_on=Depends(RoleVerify(['admin'])), db: Session = Depends(get_db)):
+    authors = db.query(models.Author).filter(models.Author.name.in_(book.authors)).all()
+    genres = db.query(models.Genre).filter(models.Genre.name.in_(book.genres)).all()
+
+    if len(authors) != len(book.authors):
+        raise HTTPException(status_code=400, detail="Some authors do not exist")
+    if len(genres) != len(book.genres):
+        raise HTTPException(status_code=400, detail="Some genres do not exist")
+    new_book = models.Book(
+        title=book.title,
+        description=book.description,
+        publication_date=book.publication_date,
+        available_copies=book.available_copies,
+        authors=authors,
+        genres=genres
+    )
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+    return schemas.BookOut(
+        id=new_book.id,
+        title=new_book.title,
+        description=new_book.description,
+        publication_date=new_book.publication_date,
+        available_copies=new_book.available_copies,
+        genres=[genre.name for genre in new_book.genres],
+        authors=[author.name for author in new_book.authors]
+    )
+
+
+@books_router.put('/{book_id}', response_model=schemas.BookOut)
+def update_book(book_id: int, book: schemas.BookUpdate, depends_on=Depends(RoleVerify(['admin'])),
+                db: Session = Depends(get_db)):
+    existing_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not existing_book:
+        raise HTTPException(status_code=404, detail='Book not found')
+
+    fields_to_update = ["title", "description", "publication_date", "available_copies"]
+    for field in fields_to_update:
+        value = getattr(book, field, None)
+        if value is not None:
+            setattr(existing_book, field, value)
+    if book.genres:
+        genres = db.query(models.Genre).filter(models.Genre.name.in_(book.genres)).all()
+        if len(genres) != len(book.genres):
+            raise HTTPException(status_code=400, detail="Some genres do not exist")
+        existing_book.genres = genres
+    if book.authors:
+        authors = db.query(models.Author).filter(models.Author.name.in_(book.authors)).all()
+        if len(authors) != len(book.authors):
+            raise HTTPException(status_code=400, detail="Some authors do not exist")
+        existing_book.authors = authors
+
+    db.commit()
+    db.refresh(existing_book)
+    return schemas.BookOut(
+        id=existing_book.id,
+        title=existing_book.title,
+        description=existing_book.description,
+        publication_date=existing_book.publication_date,
+        available_copies=existing_book.available_copies,
+        genres=[genre.name for genre in existing_book.genres],
+        authors=[author.name for author in existing_book.authors]
+    )
+
+
+@books_router.delete('/{book_id}', response_model=schemas.BookOut)
+def delete_book(book_id: int, depends_on=Depends(RoleVerify(['admin'])), db: Session = Depends(get_db)):
+    existing_book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not existing_book:
+        raise HTTPException(status_code=404, detail='Book not found')
+    db.delete(existing_book)
+    db.commit()
+    return schemas.BookOut(
+        id=existing_book.id,
+        title=existing_book.title,
+        description=existing_book.description,
+        publication_date=existing_book.publication_date,
+        available_copies=existing_book.available_copies,
+        genres=[genre.name for genre in existing_book.genres],
+        authors=[author.name for author in existing_book.authors]
+    )
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.api.endpoints.book import books_router
 from app.api.endpoints.user import auth_router
 
 app = FastAPI()
 app.include_router(auth_router, tags=["auth"])
+app.include_router(books_router, prefix='/books', tags=['books'])
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app')
