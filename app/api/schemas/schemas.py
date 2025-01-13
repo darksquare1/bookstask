2558d255068b79bc34b6fb5747e7b60da7bcb453
depressedkid <nekit05@list.ru>
2025-01-13 19:33:04 +1000

    ADD: Pydantic schemas for books

--- app/api/schemas/schemas.py
+++ app/api/schemas/schemas.py
@@ -1,3 +1,6 @@
+from datetime import date
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 
@@ -10,3 +13,29 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     username: str
     password: str
+
+
+class BookBase(BaseModel):
+    title: str
+    description: Optional[str] = None
+    publication_date: Optional[date] = None
+    available_copies: int
+
+
+class BookCreate(BookBase):
+    genres: list[str]
+    authors: list[str]
+
+
+class BookOut(BookBase):
+    id: int
+    genres: list[str]
+    authors: list[str]
+
+
+class BookUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    publication_date: Optional[date] = None
+    available_copies: Optional[int] = None
+    genres: Optional[list[str]] = None
